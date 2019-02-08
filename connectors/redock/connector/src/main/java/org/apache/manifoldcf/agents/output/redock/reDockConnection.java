81aad7bb35eac026ad0bd2ae2dfb939de29e64d0
Pierre-Olivier Charlebois <pocharlebois@redock.com>
2019-02-08 12:29:57 -0500

    reDock Ouput Connector for ManifoldCF First Commit

---
+++ connectors/redock/connector/src/main/java/org/apache/manifoldcf/agents/output/redock/reDockConnection.java
@@ -0,0 +1,289 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.manifoldcf.agents.output.redock;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.ContentType;
+import org.apache.manifoldcf.agents.interfaces.IOutputHistoryActivity;
+import org.apache.manifoldcf.agents.interfaces.ServiceInterruption;
+import org.apache.manifoldcf.core.interfaces.ManifoldCFException;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+public class reDockConnection {
+
+  public enum Result {
+    OK, NOT_FOUND_ON_ENDPOINT, ERROR, UNKNOWN
+  }
+
+  protected final reDockConfig config;
+
+  private final HttpClient client;
+  private final String serverLocation;
+
+  private String resultDescription = "";
+  private String callUrlSnippet = null;
+  private String response = null;
+  private String resultCode = null;
+  private Result result = Result.UNKNOWN;
+
+  protected final static String jsonException = "\"error\"";
+  protected final ObjectMapper objectMapper;
+
+  protected reDockConnection(reDockConfig config, HttpClient client) {
+    this.config = config;
+    this.client = client;
+    this.serverLocation = config.getServerLocation();
+    this.objectMapper = new ObjectMapper();
+  }
+
+  protected StringBuffer getApiUrl() {
+    StringBuffer url = new StringBuffer(serverLocation);
+    url.append("/syncdocument/");
+    callUrlSnippet = url.toString();
+    return url;
+  }
+
+  protected static class CallThread extends Thread {
+    protected final HttpClient client;
+    protected final HttpRequestBase method;
+    protected int resultCode = -1;
+    protected String response = null;
+    protected Throwable exception = null;
+
+    public CallThread(HttpClient client, HttpRequestBase method) {
+      this.client = client;
+      this.method = method;
+      setDaemon(true);
+    }
+
+    @Override
+    public void run() {
+      try {
+        try {
+          HttpResponse resp = client.execute(method);
+          resultCode = resp.getStatusLine().getStatusCode();
+          response = getResponseBodyAsString(resp.getEntity());
+        } finally {
+          method.abort();
+        }
+      } catch (java.net.SocketTimeoutException e) {
+        exception = e;
+      } catch (InterruptedIOException e) {
+        // Just exit
+      } catch (Throwable e) {
+        exception = e;
+      }
+    }
+
+    public void finishUp()
+      throws HttpException, IOException, InterruptedException {
+      join();
+      Throwable t = exception;
+      if (t != null) {
+        if (t instanceof HttpException)
+          throw (HttpException) t;
+        else if (t instanceof IOException)
+          throw (IOException) t;
+        else if (t instanceof RuntimeException)
+          throw (RuntimeException) t;
+        else if (t instanceof Error)
+          throw (Error) t;
+        else
+          throw new RuntimeException("Unexpected exception thrown: " + t.getMessage(), t);
+      }
+    }
+
+    public int getResultCode() {
+      return resultCode;
+    }
+
+    public String getResponse() {
+      return response;
+    }
+
+    public Throwable getException() {
+      return exception;
+    }
+  }
+
+  /** Call ElasticSearch.
+   *@return false if there was a "rejection".
+   */
+  protected boolean call(HttpRequestBase method)
+    throws ManifoldCFException, ServiceInterruption {
+    CallThread ct = new CallThread(client, method);
+    try {
+      ct.start();
+      try {
+        ct.finishUp();
+        response = ct.getResponse();
+        return handleResultCode(ct.getResultCode(), response, method.getMethod());
+      } catch (InterruptedException e) {
+        ct.interrupt();
+        throw new ManifoldCFException("Interrupted: " + e.getMessage(), e, ManifoldCFException.INTERRUPTED);
+      }
+    } catch (HttpException e) {
+      handleHttpException(e);
+      return false;
+    } catch (IOException e) {
+      handleIOException(e);
+      return false;
+    }
+  }
+
+  protected boolean handleResultCode(int code, String response, String method)
+    throws ManifoldCFException, ServiceInterruption {
+    if (code == 200 || code == 201) {
+      setResult("OK", Result.OK, null);
+      return true;
+    } else if (code == 404 && method == "DELETE") {
+      // The file was not found on the endpoint, perhaps because it was deleted by another mean.
+      // Do not halt the job altogether, simply flag the file for audit purposes but carry-on job.
+      setResult("FILE_NOT_FOUND", Result.NOT_FOUND_ON_ENDPOINT, "File does not exist on the endpoint.");
+      return true;
+    } else if (code == 404) {
+      setResult(IOutputHistoryActivity.HTTP_ERROR, Result.ERROR, "Page not found: " + response);
+      throw new ManifoldCFException("Server/page not found");
+    } else if (code >= 400 && code < 500) {
+      setResult(IOutputHistoryActivity.HTTP_ERROR, Result.ERROR, "HTTP code = " + code + ", Response = " + response);
+      return false;
+    } else if (code >= 500 && code < 600) {
+      setResult(IOutputHistoryActivity.HTTP_ERROR, Result.ERROR, "Server exception: " + response);
+      long currentTime = System.currentTimeMillis();
+      throw new ServiceInterruption("Server exception: " + response,
+        new ManifoldCFException(response),
+        currentTime + 300000L,
+        currentTime + 20L * 60000L,
+        -1,
+        false);
+    }
+    setResult(IOutputHistoryActivity.HTTP_ERROR, Result.UNKNOWN, "HTTP code = " + code + ", Response = " + response);
+    throw new ManifoldCFException("Unexpected HTTP result code: " + code + ": " + response);
+  }
+
+  protected void handleHttpException(HttpException e)
+    throws ManifoldCFException, ServiceInterruption {
+    setResult(e.getClass().getSimpleName().toUpperCase(Locale.ROOT), Result.ERROR, e.getMessage());
+    throw new ManifoldCFException(e);
+  }
+
+  protected void handleIOException(IOException e)
+    throws ManifoldCFException, ServiceInterruption {
+    if (e instanceof java.io.InterruptedIOException && !(e instanceof java.net.SocketTimeoutException))
+      throw new ManifoldCFException(e.getMessage(), ManifoldCFException.INTERRUPTED);
+    setResult(e.getClass().getSimpleName().toUpperCase(Locale.ROOT), Result.ERROR, e.getMessage());
+    long currentTime = System.currentTimeMillis();
+    // All IO exceptions are treated as service interruptions, retried for an hour
+    throw new ServiceInterruption("IO exception: " + e.getMessage(), e,
+      currentTime + 60000L,
+      currentTime + 1L * 60L * 60000L,
+      -1,
+      true);
+  }
+
+  private static String getResponseBodyAsString(HttpEntity entity)
+    throws IOException, HttpException {
+    InputStream is = entity.getContent();
+    if (is != null) {
+      try {
+        Charset charSet;
+        try {
+          ContentType ct = ContentType.get(entity);
+          if (ct == null)
+            charSet = StandardCharsets.UTF_8;
+          else
+            charSet = ct.getCharset();
+        } catch (ParseException e) {
+          charSet = StandardCharsets.UTF_8;
+        }
+        char[] buffer = new char[65536];
+        Reader r = new InputStreamReader(is, charSet);
+        Writer w = new StringWriter();
+        try {
+          while (true) {
+            int amt = r.read(buffer);
+            if (amt == -1)
+              break;
+            w.write(buffer, 0, amt);
+          }
+        } finally {
+          w.flush();
+        }
+        return w.toString();
+      } finally {
+        is.close();
+      }
+    }
+    return "";
+  }
+
+  protected String checkJson(String jsonQuery) throws ManifoldCFException {
+    String result = null;
+    if (response != null) {
+      String[] tokens = response.replaceAll("\\{", "").replaceAll("\\}", "")
+        .split(",");
+      for (String token : tokens)
+        if (token.contains(jsonQuery))
+          result = token.substring(token.indexOf(":") + 1);
+    }
+    return result;
+  }
+
+  protected void setResult(String resultCode, Result res, String desc) {
+    if (res != null)
+      result = res;
+    if (desc != null)
+      if (desc.length() > 0)
+        resultDescription = desc;
+    setResultCode(resultCode);
+  }
+
+  public String getResultDescription() {
+    return resultDescription;
+  }
+
+  protected String getResponse() {
+    return response;
+  }
+
+  public Result getResult() {
+    return result;
+  }
+
+  public String getCallUrlSnippet() {
+    return callUrlSnippet;
+  }
+
+  public String getResultCode() {
+    return resultCode;
+  }
+
+  public void setResultCode(String resultCode) {
+    this.resultCode = resultCode;
+  }
+}
---
+++ connectors/redock/connector/src/main/java/org/apache/manifoldcf/agents/output/redock/reDockConnector.java
@@ -0,0 +1,377 @@
+/* $Id: reDockConnector.java 988245 2010-08-23 18:39:35Z kwright $ */
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.manifoldcf.agents.output.redock;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HttpRequestExecutor;
+import org.apache.manifoldcf.agents.incrementalingest.IncrementalIngester;
+import org.apache.manifoldcf.agents.output.BaseOutputConnector;
+import org.apache.manifoldcf.core.interfaces.*;
+import org.apache.manifoldcf.agents.interfaces.*;
+import org.apache.manifoldcf.crawler.system.WorkerThread;
+
+import java.util.*;
+import java.io.*;
+
+/** This is a reDock output connector that sends the document to a reDock endpoint.
+ */
+public class reDockConnector extends BaseOutputConnector {
+  // Activities we log
+
+  /** Source name */
+  public final static String REDOCK_SOURCE_NAME = "reDock ManifoldCF Output Connector";
+  /** Ingestion activity */
+  public final static String INGEST_ACTIVITY = "reDock ingest";
+  /** Document removal activity */
+  public final static String REMOVE_ACTIVITY = "reDock deletion";
+  /** Job notify activity */
+  public final static String JOB_COMPLETE_ACTIVITY = "output notification";
+
+  private final static String REDOCK_TAB_SERVER = "reDockConnector.EndPoint";
+
+  /** Forward to the javascript to check the configuration parameters */
+  private static final String EDIT_CONFIG_HEADER_FORWARD = "editConfiguration.js";
+
+  /** Forward to the HTML template to edit the configuration parameters */
+  private static final String EDIT_CONFIG_FORWARD_SERVER = "editConfiguration_Server.html";
+
+  /** Forward to the HTML template to view the configuration parameters */
+  private static final String VIEW_CONFIG_FORWARD = "viewConfiguration.html";
+
+  /** Connection expiration interval */
+  private static final long EXPIRATION_INTERVAL = 60000L;
+
+  private HttpClientConnectionManager connectionManager = null;
+  private HttpClient client = null;
+  private long expirationTime = -1L;
+
+
+  /** Constructor.
+   */
+  public reDockConnector() {
+  }
+
+  /** Return the list of activities that this connector supports (i.e. writes into the log).
+   *@return the list.
+   */
+  @Override
+  public String[] getActivitiesList() {
+    return new String[]{INGEST_ACTIVITY, REMOVE_ACTIVITY, JOB_COMPLETE_ACTIVITY};
+  }
+
+  /** Connect.
+   *@param configParams is the set of configuration parameters, which
+   * in this case describe the target appliance, basic auth configuration, etc.  (This formerly came
+   * out of the ini file.)
+   */
+  @Override
+  public void connect(ConfigParams configParams) {
+    super.connect(configParams);
+  }
+
+  protected HttpClient getSession() {
+    if (client == null) {
+      int socketTimeout = 900000;
+      int connectionTimeout = 60000;
+
+      // Load configuration from parameters
+      final reDockConfig config = new reDockConfig(params);
+
+      // Set up connection manager
+      PoolingHttpClientConnectionManager poolingConnectionManager = new PoolingHttpClientConnectionManager(RegistryBuilder.<ConnectionSocketFactory>create()
+        .register("http", PlainConnectionSocketFactory.getSocketFactory())
+        .register("https",  SSLConnectionSocketFactory.getSocketFactory())
+        .build());
+      poolingConnectionManager.setDefaultMaxPerRoute(1);
+      poolingConnectionManager.setValidateAfterInactivity(2000);
+      poolingConnectionManager.setDefaultSocketConfig(SocketConfig.custom()
+        .setTcpNoDelay(true)
+        .setSoTimeout(socketTimeout)
+        .build());
+      connectionManager = poolingConnectionManager;
+
+      RequestConfig.Builder requestBuilder = RequestConfig.custom()
+        .setCircularRedirectsAllowed(true)
+        .setSocketTimeout(socketTimeout)
+        .setExpectContinueEnabled(true)
+        .setConnectTimeout(connectionTimeout)
+        .setConnectionRequestTimeout(socketTimeout);
+
+      client = HttpClients.custom()
+        .setConnectionManager(connectionManager)
+        .setMaxConnTotal(1)
+        .disableAutomaticRetries()
+        .setDefaultRequestConfig(requestBuilder.build())
+        .setRequestExecutor(new HttpRequestExecutor(socketTimeout))
+        // Provide
+        .setDefaultHeaders(Arrays.asList(
+          new BasicHeader(HttpHeaders.AUTHORIZATION, "Bearer " + config.getToken()),
+          new BasicHeader("X-RedockAuthMode", "REST-API")
+        ))
+        .build();
+    }
+
+    expirationTime = System.currentTimeMillis() + EXPIRATION_INTERVAL;
+    return client;
+  }
+
+  protected void closeSession() {
+    if (connectionManager != null) {
+      connectionManager.shutdown();
+      connectionManager = null;
+    }
+    client = null;
+    expirationTime = -1L;
+  }
+
+  /** This method is called to assess whether to count this connector instance should
+   * actually be counted as being connected.
+   *@return true if the connector instance is actually connected.
+   */
+  @Override
+  public boolean isConnected() {
+    return connectionManager != null;
+  }
+
+  @Override
+  public void disconnect()
+    throws ManifoldCFException {
+    super.disconnect();
+    closeSession();
+  }
+
+  @Override
+  public void poll()
+    throws ManifoldCFException {
+    super.poll();
+    if (connectionManager != null) {
+      if (System.currentTimeMillis() > expirationTime) {
+        closeSession();
+      }
+    }
+  }
+
+  /** Get an output version string, given an output specification.  The output version string is used to uniquely describe the pertinent details of
+   * the output specification and the configuration, to allow the Connector Framework to determine whether a document will need to be output again.
+   * Note that the contents of the document cannot be considered by this method, and that a different version string (defined in IRepositoryConnector)
+   * is used to describe the version of the actual document.
+   *
+   * This method presumes that the connector object has been configured, and it is thus able to communicate with the output data store should that be
+   * necessary.
+   *@param spec is the current output specification for the job that is doing the crawling.
+   *@return a string, of unlimited length, which uniquely describes output configuration and specification in such a way that if two such strings are equal,
+   * the document will not need to be sent again to the output data store.
+   */
+  @Override
+  public VersionContext getPipelineDescription(Specification spec) {
+    return new VersionContext("", params, spec);
+  }
+
+  /** Add (or replace) a document in the output data store using the connector.
+   * This method presumes that the connector object has been configured, and it is thus able to communicate with the output data store should that be
+   * necessary.
+   * The OutputSpecification is *not* provided to this method, because the goal is consistency, and if output is done it must be consistent with the
+   * output description, since that was what was partly used to determine if output should be taking place.  So it may be necessary for this method to decode
+   * an output description string in order to determine what should be done.
+   *@param documentURI is the URI of the document.  The URI is presumed to be the unique identifier which the output data store will use to process
+   * and serve the document.  This URI is constructed by the repository connector which fetches the document, and is thus universal across all output connectors.
+   *@param outputDescription is the description string that was constructed for this document by the getOutputDescription() method.
+   *@param document is the document data to be processed (handed to the output data store).
+   *@param authorityNameString is the name of the authority responsible for authorizing any access tokens passed in with the repository document.  May be null.
+   *@param activities is the handle to an object that the implementer of an output connector may use to perform operations, such as logging processing activity.
+   *@return the document status (accepted or permanently rejected).
+   */
+  @Override
+  public int addOrReplaceDocumentWithException(String documentURI, VersionContext outputDescription, RepositoryDocument document, String authorityNameString, IOutputAddActivity activities)
+    throws ManifoldCFException, ServiceInterruption, IOException {
+
+    // Reject temporary working documents that starts with ~
+    if (document.getFileName().startsWith("~"))
+      return DOCUMENTSTATUS_REJECTED;
+
+    // Establish a session
+    HttpClient client = getSession();
+    reDockConfig config = getConfigParameters(null);
+    reDockAction reDock = new reDockAction(client, config);
+
+    try
+    {
+      reDock.executePUT(documentURI, document);
+
+      if (reDock.getResult() != reDockConnection.Result.OK)
+        return DOCUMENTSTATUS_REJECTED;
+
+      activities.recordActivity(null, INGEST_ACTIVITY, document.getBinaryLength(), documentURI, "OK", reDock.getResultDescription());
+    }
+    catch(Exception e)
+    {
+      activities.recordActivity(null, INGEST_ACTIVITY, document.getBinaryLength(), documentURI, "EXCEPTION", e.toString());
+    }
+    return DOCUMENTSTATUS_ACCEPTED;
+  }
+
+  /** Remove a document using the connector.
+   * Note that the last outputDescription is included, since it may be necessary for the connector to use such information to know how to properly remove the document.
+   *@param documentURI is the URI of the document.  The URI is presumed to be the unique identifier which the output data store will use to process
+   * and serve the document.  This URI is constructed by the repository connector which fetches the document, and is thus universal across all output connectors.
+   *@param outputDescription is the last description string that was constructed for this document by the getOutputDescription() method above.
+   *@param activities is the handle to an object that the implementer of an output connector may use to perform operations, such as logging processing activity.
+   */
+  @Override
+  public void removeDocument(String documentURI, String outputDescription, IOutputRemoveActivity activities)
+    throws ManifoldCFException, ServiceInterruption {
+    // Establish a session
+    HttpClient client = getSession();
+    reDockConfig config = getConfigParameters(null);
+    reDockAction reDock = new reDockAction(client, config);
+
+    try
+    {
+      reDock.executeDELETE(documentURI);
+    }
+    finally
+    {
+      activities.recordActivity(null, REMOVE_ACTIVITY, null, documentURI, reDock.getResult().toString(), null);
+    }
+  }
+
+  /** Notify the connector of a completed job.
+   * This is meant to allow the connector to flush any internal data structures it has been keeping around, or to tell the output repository that this
+   * is a good time to synchronize things.  It is called whenever a job is either completed or aborted.
+   *@param activities is the handle to an object that the implementer of an output connector may use to perform operations, such as logging processing activity.
+   */
+  @Override
+  public void noteJobComplete(IOutputNotifyActivity activities)
+    throws ManifoldCFException, ServiceInterruption {
+    // Establish a session
+    HttpClient client = getSession();
+    reDockConfig config = getConfigParameters(null);
+    reDockAction reDock = new reDockAction(client, config);
+
+    activities.recordActivity(null, JOB_COMPLETE_ACTIVITY, null, "", "OK", null);
+  }
+
+  /** Read the content of a resource, replace the variable ${PARAMNAME} with the
+   * value and copy it to the out.
+   *
+   * @param resName
+   * @param out
+   * @throws ManifoldCFException */
+  private static void outputResource(String resName, IHTTPOutput out,
+                                     Locale locale, reDockParam params,
+                                     String tabName, Integer sequenceNumber, Integer currentSequenceNumber) throws ManifoldCFException {
+    Map<String, Object> paramMap = null;
+    if (params != null) {
+      paramMap = params.buildMap(out);
+      if (tabName != null) {
+        paramMap.put("TabName", tabName);
+      }
+      if (currentSequenceNumber != null)
+        paramMap.put("SelectedNum", currentSequenceNumber.toString());
+    } else {
+      paramMap = new HashMap<String, Object>();
+    }
+    if (sequenceNumber != null)
+      paramMap.put("SeqNum", sequenceNumber.toString());
+
+    Messages.outputResourceWithVelocity(out, locale, resName, paramMap);
+  }
+
+  @Override
+  public void outputConfigurationHeader(IThreadContext threadContext,
+                                        IHTTPOutput out, Locale locale, ConfigParams parameters,
+                                        List<String> tabsArray) throws ManifoldCFException, IOException {
+    super.outputConfigurationHeader(threadContext, out, locale, parameters, tabsArray);
+    tabsArray.add(Messages.getString(locale, REDOCK_TAB_SERVER));
+    outputResource(EDIT_CONFIG_HEADER_FORWARD, out, locale, null, null, null, null);
+  }
+
+  @Override
+  public void outputConfigurationBody(IThreadContext threadContext,
+                                      IHTTPOutput out, Locale locale, ConfigParams parameters, String tabName)
+    throws ManifoldCFException, IOException {
+    try {
+      super.outputConfigurationBody(threadContext, out, locale, parameters, tabName);
+      reDockConfig config = this.getConfigParameters(parameters);
+      outputResource(EDIT_CONFIG_FORWARD_SERVER, out, locale, config, tabName, null, null);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /** Build a Set of reDock parameters. If configParams is null,
+   * getConfiguration() is used.
+   *
+   * @param configParams */
+  final private reDockConfig getConfigParameters(
+    ConfigParams configParams) {
+    if (configParams == null)
+      configParams = getConfiguration();
+    return new reDockConfig(configParams);
+  }
+
+  @Override
+  public void viewConfiguration(IThreadContext threadContext, IHTTPOutput out,
+                                Locale locale, ConfigParams parameters)
+    throws ManifoldCFException {
+    outputResource(VIEW_CONFIG_FORWARD, out, locale,
+      getConfigParameters(parameters), null, null, null);
+  }
+
+  @Override
+  public String processConfigurationPost(IThreadContext threadContext,
+                                         IPostParameters variableContext, ConfigParams parameters)
+    throws ManifoldCFException {
+    return reDockConfig.contextToConfig(threadContext, variableContext, parameters);
+  }
+
+  /** Test the connection.  Returns a string describing the connection integrity.
+   *@return the connection's status as a displayable string.
+   */
+  @Override
+  public String check() throws ManifoldCFException {
+    HttpClient client = getSession();
+    reDockAction reDock = new reDockAction(client, getConfigParameters(null));
+    try {
+      reDock.executeGET();
+      String resultName = reDock.getResult().name();
+      if (resultName.equals("OK")) {
+        JsonNode responseNode = reDock.getResponseJsonNode();
+        String clientName = responseNode.findValue("client").asText();
+        String environment = responseNode.findValue("environment").asText();
+        return super.check() + " with ClientName = " + clientName + ", and Environment = " + environment + ".";
+      }
+      return resultName + " " + reDock.getResultDescription();
+    } catch (ServiceInterruption e) {
+      return "Transient exception: " + e.getMessage();
+    }
+  }
+}
